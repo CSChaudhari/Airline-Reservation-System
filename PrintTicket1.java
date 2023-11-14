d692bf1b483468f2ae8a471ba6a1a38f7481d864
CSChaudhari <123755257+CSChaudhari@users.noreply.github.com>
2023-11-14 15:25:27 +0530

    Add files via upload

---
+++ PrintTicket1.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class PrintTicket1 extends JFrame {
+    public PrintTicket1(String sFrom, String sTo, String sClass, Integer iAdult, Integer iChildren, Integer iInfant, String sBookingDate, Integer iPrice, String sTime) {
+        Container c = getContentPane();
+        c.setLayout(new BorderLayout());
+        Random random = new Random();
+        JPanel Panel2 = new JPanel(null);
+        Panel2.setPreferredSize(new Dimension(500, 200));
+
+        JLabel LTitle = new JLabel("<html><b><font color=\"#C71585\",size=\"7\">Airline Ticket</font></b></html>");
+        JLabel LFrom = new JLabel("<html><b><font color=\"#000000\">From: </font></b><font color=\"#778899\">" + sFrom + "</font></html>");
+        JLabel LTo = new JLabel("<html><font color=\"#000000\">To: </font><font color=\"#778899\">" + sTo + "</font></html>");
+        JLabel LClass = new JLabel("<html><font color=\"#000000\">Class: </font><font color=\"#778899\">" + sClass + "</font></html>");
+        JLabel LBookingDate = new JLabel("<html><font color=\"#000000\">Traveling Date: </font><font color=\"#778899\">" + sBookingDate + "</font></html>");
+        JLabel LPrice = new JLabel("<html><font color=\"#000000\">Total Price: </font><font color=\"#778899\">" + iPrice + "</font></html>");
+        JLabel LTime = new JLabel("<html><font color=\"#000000\">Departure Time: </font><font color=\"#778899\">" + sTime + "</font></html>");
+        JLabel LAdult = new JLabel("<html><font color=\"#000000\">Adult: </font><font color=\"#778899\">" + iAdult + "</font></html>");
+        JLabel LChildren = new JLabel("<html><font color=\"#000000\">Children: </font><font color=\"#778899\">" + iChildren + "</font></html>");
+        JLabel LInfant = new JLabel("<html><font color=\"#000000\">Infant: </font><font color=\"#778899\">" + iInfant + "</font></html>");
+        JLabel LWishes = new JLabel("<html><body><i><font color=\"#D2B48C\">Wish you a happy journey</font></i></body></html>");
+
+        JLabel LTicketNo = new JLabel("<html><font color=\"#000000\">Ticket NO: </font><font color=\"#778899\">" + random.nextInt() + "</font></html>");
+        JLabel LBookedBy = new JLabel("<html><font color=\"#000000\">Booked By: Chaitanya Chaudhari</font></html>");
+
+        JLabel LEmpty = new JLabel("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        JLabel LDemo = new JLabel("<html><u><font color=\"#8FBC8F\">Airline-Project Developed By</font></u></html>");
+        JLabel LChaitanya = new JLabel("<html><i><font color=\"#8FBC8F\">Chaitanya Chaudhari</font></i></html>");
+
+        LTitle.setBounds(170, 15, 500, 45);
+        LFrom.setBounds(20, 80, 300, 20);
+        LTo.setBounds(20, 125, 300, 20);
+        LClass.setBounds(20, 170, 300, 20);
+        LBookingDate.setBounds(20, 215, 300, 20);
+        LPrice.setBounds(20, 260, 300, 20);
+        LTime.setBounds(20, 305, 300, 20);
+        LAdult.setBounds(20, 345, 300, 20);
+        LChildren.setBounds(20, 385, 300, 20);
+        LInfant.setBounds(20, 430, 300, 20);
+        LWishes.setBounds(530, 435, 300, 20);
+        LTicketNo.setBounds(320, 80, 300, 20);
+        LBookedBy.setBounds(320, 125, 300, 20);
+        LEmpty.setBounds(3, 445, 1000, 20);
+        LDemo.setBounds(280, 465, 300, 20);
+        LChaitanya.setBounds(285, 485, 300, 20);
+
+        Panel2.add(LTitle);
+        Panel2.add(LFrom);
+        Panel2.add(LTo);
+        Panel2.add(LClass);
+        Panel2.add(LBookingDate);
+        Panel2.add(LAdult);
+        Panel2.add(LChildren);
+        Panel2.add(LInfant);
+        Panel2.add(LPrice);
+        Panel2.add(LTime);
+        Panel2.add(LWishes);
+        Panel2.add(LTicketNo);
+        Panel2.add(LBookedBy);
+        Panel2.add(LEmpty);
+        Panel2.add(LDemo);
+        Panel2.add(LChaitanya);
+
+        Panel2.setBackground(Color.WHITE);
+
+        c.add(Panel2, BorderLayout.CENTER);
+
+        setSize(700, 650);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new PrintTicket1("New York", "Los Angeles", "Business", 2, 1, 0, "2023-10-16", 800, "15:00");
+        });
+    }
+}
